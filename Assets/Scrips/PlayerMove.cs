7bdeacdb7c4833753402b40dedbfda87564c87f6
AnushkaX <anushka.mutusinghe@gmail.com>
2021-06-12 16:05:56 +0530

    player movements added

---
+++ Assets/Scrips/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 100;
+
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        GetComponent<CharacterController>().SimpleMove(movement * Time.deltaTime * moveSpeed);
+    }
+}
