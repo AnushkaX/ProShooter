4a11684888257be6fe276af12253eca55c6bf5a5
AnushkaX <anushka.mutusinghe@gmail.com>
2021-06-13 13:48:53 +0530

    gun fire added

---
+++ Assets/Scrips/Gun.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.5f, 1.5f)]
+    private float fireRate = 1;
+
+    [SerializeField]
+    [Range(1, 10)]
+    private int damage = 1;
+
+    [SerializeField]
+    private Transform firePoint;
+
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= fireRate)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                timer = 0f;
+                FireGun();
+            }
+        }
+    }
+
+    private void FireGun()
+    {
+        Debug.DrawRay(firePoint.position, firePoint.forward * 100, Color.red, 2f);
+
+        Ray ray = new Ray(firePoint.position, firePoint.forward);
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(ray, out hitInfo, 100))
+        {
+            Destroy(hitInfo.collider.gameObject);
+
+        }
+
+    }
+}
